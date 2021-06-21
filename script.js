55f0eec906a288bc47b0a9c714cfff8ce0914c2a
Shawn Lee <shawnzap@gmail.com>
2021-06-21 20:12:56 +0800

    bootstrap carousel for testimonials

--- script.js
+++ script.js
@@ -13,30 +13,3 @@ function clearActive() {
     links[i].className = "nav-link";
   }
 }
-
-//Testimonials Slideshow
-var slides = document.getElementsByClassName("slides");
-var dots = document.getElementsByClassName("dot");
-var slideIndex = 2;
-var tOut;
-
-function showSlides() {
-  var i;
-  for (i = 0; i < slides.length; i++) {
-    slides[i].classList.add("hidden");
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].classList.remove("dot-active");
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].classList.remove("hidden");
-  dots[slideIndex-1].classList.add("dot-active");
-  tOut = setTimeout(showSlides, 15000);
-}
-
-function go_to(n) {
-  clearTimeout(tOut)
-  slideIndex = n - 1;
-  showSlides();
-}
