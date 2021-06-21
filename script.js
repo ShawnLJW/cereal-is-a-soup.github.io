bea614ccb0ff3134e6b895d4233e443de6199b98
Shawn Lee <shawnzap@gmail.com>
2021-06-21 11:09:25 +0800

    bootstrap container implementation
    in projects page

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
-var slideIndex = 2;
-var activePage = 0;
-var to;
-
-function go_to(n) {
-  clearTimeout(to)
-  slideIndex = n - 1;
-  showSlides();
-}
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].classList.add("hidden");
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].classList.remove("active");
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].classList.remove("hidden");
-  dots[slideIndex-1].classList.add("active");
-  to = setTimeout(showSlides, 15000);
-}
-
-function expandNav() {
-  var x = document.getElementById("dropdown");
-  if (x.className === "dropdown") {
-    x.className += " dropdownActive";
-  } else {
-    x.className = "dropdown";
-  }
-}
-
+//Navigation shows active page
 var links = document.getElementsByClassName("nav-link");
+var activePage = 0;
 
 function activate(i) {
   links[activePage].className = "nav-link";
@@ -46,4 +12,31 @@ function clearActive() {
   for (var i = 0; i < links.length; i++) {
     links[i].className = "nav-link";
   }
+}
+
+//Testimonials Slideshow
+var slides = document.getElementsByClassName("slides");
+var dots = document.getElementsByClassName("dot");
+var slideIndex = 2;
+var tOut;
+
+function showSlides() {
+  var i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.add("hidden");
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("dot-active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].classList.remove("hidden");
+  dots[slideIndex-1].classList.add("dot-active");
+  tOut = setTimeout(showSlides, 15000);
+}
+
+function go_to(n) {
+  clearTimeout(tOut)
+  slideIndex = n - 1;
+  showSlides();
 }
